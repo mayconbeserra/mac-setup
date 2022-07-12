fc7b37fdb519ab12fcfb1a9bedf27695bbe93b17
Maycon Beserra <mayconbeserra@hotmail.com>
2022-07-12 13:49:20 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -78,6 +78,7 @@ brew install --cask docker
 brew install --cask timeular
 brew install --cask keepassxc
 brew install --cask drawio
+brew install --cask robo-3t
 
 # Communication
 brew install --cask skype
